56fcef4ff9afdab66d7920cb899c42c420ade838
afzaltk <afzalmoideen123@gmail.com>
2017-07-30 16:15:08 +1000

    Initial

--- src/fit5042/tutex/RealEstateAgencyPresenter.java
+++ src/fit5042/tutex/RealEstateAgencyPresenter.java
@@ -5,20 +5,31 @@
  */
 package fit5042.tutex;
 
+import fit5042.tutex.gui.RealEstateAgencyGUI;
+import fit5042.tutex.gui.SimpleGUIImpl;
+import fit5042.tutex.repository.PropertyRepository;
+import fit5042.tutex.repository.PropertyRepositoryFactory;
+import fit5042.tutex.repository.entities.Property;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * TODO Exercise 1B Step 2 This class will be used for Exercise 1B.
  *
  * Only start this upon the completion of Exercise 1A
- * 
+ *
  * Before you start this exercise, you will need an understanding of the MVP
- * Model View Presenter design pattern used. 
- * 
- * Please refer to the provided example to aid you in
- * completing this exercise. 
- * 
- * This is the presenter. It will use both the Model and the View 
- * 
- * 
+ * Model View Presenter design pattern used.
+ *
+ * Please refer to the provided example to aid you in completing this exercise.
+ *
+ * This is the presenter. It will use both the Model and the View
+ *
+ *
  *
  * This class implements ActionListener.
  *
@@ -26,6 +37,101 @@ package fit5042.tutex;
  *
  * @author Jian
  */
-public class RealEstateAgencyPresenter {
+public class RealEstateAgencyPresenter implements ActionListener {
+
+    private String name;
+    private PropertyRepository propertyRepository;
+    private RealEstateAgencyGUI gui;
+    private int propertyId;
+
+    public RealEstateAgencyPresenter(String name) {
+        this.name = name;
+        try {
+            this.propertyRepository = PropertyRepositoryFactory.getInstance();
+
+            this.propertyRepository.addProperty(new Property(1, "4/44 lansdowne road, St Kilda East VIC 3183, Australia", 2, 150, 420000));
+            this.propertyRepository.addProperty(new Property(2, "9 Toorak road, Melbourne VIC 3186, Australia", 2, 150, 420000));
+            this.propertyRepository.addProperty(new Property(3, "71 High Street road, Melbourne VIC 3176, Australia", 2, 150, 420000));
+            this.propertyRepository.addProperty(new Property(4, "22 Flinders Street, Melbourne VIC 3003, Australia", 2, 150, 420000));
+            this.propertyRepository.addProperty(new Property(5, "41 Kings road, Melbourne VIC 3234, Australia", 2, 150, 420000));
+
+        } catch (Exception ex) {
+            Logger.getLogger(RealEstateAgencyPresenter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        RealEstateAgencyPresenter presenter = new RealEstateAgencyPresenter("Real Estate Agency");
+        presenter.runMethod();
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == gui.getAddButton()) {
+            this.addProperty();
+        } else if (e.getSource() == gui.getSearchButton()) {
+            this.searchProperty();
+        } else if (e.getSource() == gui.getViewButton()) {
+            this.viewAllProperty();
+        } else {
+            System.exit(0);
+        }
+    }
+
+    private void runMethod() {
+        gui = new SimpleGUIImpl(this);
+    }
+
+    private void addProperty() {
+        try {
+            Property property = gui.getPropertyDetails();
+            propertyRepository.addProperty(property);
+            gui.displayPopUpMessage("Property added successfully...!!");
+            viewAllProperty();
+            gui.clearTextFields();
+        } catch (Exception ex) {
+           gui.displayPopUpMessage("Failed to add Property " + ex.getMessage());
+        }
+
+    }
+
+    private void searchProperty() {
+        try {
+            propertyId = gui.getPropertyId();
+            Property propertyDetail = propertyRepository.searchPropertyById(propertyId);
+            if (propertyDetail == null) {
+                gui.displayPopUpMessage("Property does not exist");
+            } else {
+                gui.displayPropertyDetails(propertyDetail);
+            }
+        } catch (Exception ex) {
+            gui.displayPopUpMessage("Failed to view Property " + ex.getMessage());
+        }
+    }
+
+    private void viewAllProperty() {
+        try {
+            gui.clearTextFields();
+            List<Property> propertyList;
+            propertyList = this.propertyRepository.getAllProperties();
+            if (propertyList.isEmpty() || propertyList == null) {
+                gui.displayPopUpMessage("No Property Found");
+            } else {
+                gui.displayPropertyDetails(propertyList);
+            }
+
+        } catch (Exception ex) {
+            gui.displayPopUpMessage("Failed to view Property " + ex.getMessage());
+        }
+    }
 
 }
--- src/fit5042/tutex/gui/SimpleGUIImpl.java
+++ src/fit5042/tutex/gui/SimpleGUIImpl.java
@@ -1,5 +1,21 @@
 package fit5042.tutex.gui;
 
+import fit5042.tutex.repository.entities.Property;
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.util.List;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
 /**
  *
  *
@@ -23,6 +39,153 @@ package fit5042.tutex.gui;
  * @author Jian
  * @version 2017-06-16
  */
-public class SimpleGUIImpl {
+public class SimpleGUIImpl extends JFrame implements RealEstateAgencyGUI{
+    
+    private final JLabel propertyIdLabel;
+    private final JLabel addressLabel;
+    private final JLabel bedroomCountLabel;
+    private final JLabel sizeLabel;
+    private final JLabel priceLabel;
+    
+    private final JTextField propertyIdField;
+    private final JTextField addressField;
+    private final JTextField bedroomCountField;
+    private final JTextField sizeField;
+    private final JTextField priceField;
+    
+    private final JTextArea dataArea;
+    
+    private final JButton addButton;
+    private final JButton searchButton;
+    private final JButton viewButton;
+    private final JButton closeButton;
+    
+    private final JPanel inputPanel;
+    private final JPanel buttonPanel;
+    
+    private int propertyId;
+
+    public SimpleGUIImpl(ActionListener actionListener) {
+        
+        propertyIdLabel=new JLabel("Property ID");
+        addressLabel=new JLabel("Address");
+        bedroomCountLabel = new JLabel("Number of Bedrooms");
+        sizeLabel = new JLabel("Size");
+        priceLabel = new JLabel("Price");
+        
+        propertyIdField = new JTextField();
+        addressField = new JTextField();
+        bedroomCountField = new JTextField();
+        sizeField = new JTextField();
+        priceField = new JTextField();
+        
+        dataArea = new JTextArea();
+        dataArea.setEditable(false);
+        
+        addButton = new JButton("Add");
+        searchButton = new JButton("Search");
+        viewButton = new JButton("View");
+        closeButton = new JButton("Close");
+        
+        closeButton.addActionListener(actionListener);
+        addButton.addActionListener(actionListener);
+        viewButton.addActionListener(actionListener);
+        searchButton.addActionListener(actionListener);
+        
+        inputPanel = new JPanel();
+        buttonPanel=new JPanel();
+        
+        
+        inputPanel.add(propertyIdLabel);
+        inputPanel.add(propertyIdField);
+        inputPanel.add(addressLabel);
+        inputPanel.add(addressField);
+        inputPanel.add(bedroomCountLabel);
+        inputPanel.add(bedroomCountField);
+        inputPanel.add(sizeLabel);
+        inputPanel.add(sizeField);
+        inputPanel.add(priceLabel);
+        inputPanel.add(priceField);
+        
+        buttonPanel.add(addButton);
+        buttonPanel.add(searchButton);
+        buttonPanel.add(viewButton);
+        buttonPanel.add(closeButton);
+       
+        Container container = this.getContentPane();
+        container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
+        container.setLayout(new BorderLayout());
+        inputPanel.setLayout(new GridLayout(5, 2));
+        container.add(inputPanel, BorderLayout.NORTH);
+        container.add(new JScrollPane(dataArea), BorderLayout.CENTER);
+        container.add(buttonPanel, BorderLayout.SOUTH);
+        buttonPanel.setLayout(new GridLayout(1, 4));
+        this.setSize(600, 540);
+        
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    @Override
+    public void clearTextFields() {
+        propertyIdField.setText("");
+        addressField.setText("");
+        bedroomCountField.setText("");
+        sizeField.setText("");
+        priceField.setText("");
+    }
+
+    @Override
+    public void displayPopUpMessage(String message) {
+        JOptionPane.showMessageDialog(this, message);
+    }
+
+    @Override
+    public void displayPropertyDetails(Property property) {
+        dataArea.setText(property.toString());
+    }
+
+    @Override
+    public void displayPropertyDetails(List<Property> properties) {
+        dataArea.setText("");
+        properties .forEach((property) -> {
+            dataArea.append(property.toString()+"\n");
+        });
+    }
+
+    @Override
+    public JButton getAddButton() {
+        return addButton;
+    }
+
+    @Override
+    public JButton getCloseButton() {
+        return closeButton;
+    }
+
+    @Override
+    public JButton getSearchButton() {
+        return searchButton;
+    }
+
+    @Override
+    public JButton getViewButton() {
+        return viewButton;
+    }
+
+    @Override
+    public Property getPropertyDetails() {
+        if (propertyIdField.getText()==null)
+            return null;
+        Property p=new Property(Integer.parseInt(propertyIdField.getText()), addressField.getText(), Integer.parseInt(bedroomCountField.getText()), Double.parseDouble(sizeField.getText()),Double.parseDouble(priceField.getText()));
+        return p;
+        
+    }
+
+    @Override
+    public int getPropertyId() {
+        propertyId=Integer.parseInt(propertyIdField.getText());
+        return propertyId;
+    }
 
 }
